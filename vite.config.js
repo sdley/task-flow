fd14f5f9470bd572ca9d03095621d8a58faa25e5
Souleymane DIALLO <sdley2020@gmail.com>
2024-11-04 22:54:36 +0000

    Mise a jour des parametres du serveur vite-react

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+// })
+  // permettre aux appareils de notre reseau d'acceder a notre apk
+  server: {
+    host: '0.0.0.0', // Ou votre adresse IP (e.g., '192.168.1.100')
+    port: 3000, // Ou le port que vous utilisez
+  },
+});
