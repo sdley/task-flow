b5a6dd30969b106faa239e4f1f2ef8145f1ae3c5
Souleymane DIALLO <sdley2020@gmail.com>
2024-11-05 13:02:24 +0000

    Ajouter une tache

--- src/components/taskContainer.jsx
+++ src/components/taskContainer.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Footer } from "./footer/footer"
 import Header from "./header/header"
 import TaskInput from "./taskInput/taskInput"
@@ -7,10 +8,43 @@ import TaskList from "./taskList/taskList"
 
 // Implementation du composant que nous utiliserons pour afficher l'integralite de notre contenu
 const TaskContainer = () => {
+
+  // creation d'un etat de la liste des taches
+  const [tasksList, setTasksList] = useState(
+    [
+      // {
+      //   id: 1,
+      //   title: "Faire mon TP d'entrepot de donnees",
+      //   completed: false
+      // },
+      // {
+      //   id: 2,
+      //   title: "Faire mon Lab CI/CD",
+      //   completed: true
+      // },
+
+    ]
+  );
+
+  // fonction d'ajout de tache
+  const addTask = (title) => {
+    const newTask = {
+      id: tasksList.length + 1,
+      title,
+      // title: title,
+      completed: false
+    };
+    // On utilise le spread operator ... pour mettre a jour notre liste de taches
+    setTasksList([...tasksList, newTask]);
+  };
+
+  console.log('taskList: ' ,tasksList);
+
   return (
     <main>
       <Header />
-      <TaskInput />
+      {/* passons la methode au composant */}
+      <TaskInput addTask={addTask} />
       <TaskList />
       <Footer />
     </main>
--- src/components/taskInput/taskInput.jsx
+++ src/components/taskInput/taskInput.jsx
@@ -1,18 +1,50 @@
 // Ce composant sera utilise pour afficher le champ de saisie de nos taches
+import { useState } from "react";
 import styles from "./taskInput.module.css"
 
-function TaskInput() {
+function TaskInput({addTask}) {
+  const [taskTitle, setTaskTitle] = useState("")
+
+  // console.log(addTask);
+
+  const handleInputChange = (e) => {
+    setTaskTitle(e.target.value);
+  };
+  // console.log(taskTitle);
+
+  const handleAddtask = (e) => {
+    // Empecher le rechargement de la page a l'envoie des donnees
+    e.preventDefault();
+    /*
+      Pour eviter d'ajouter des taches vides dans notre application,
+      nous utilisons la methode trim() de JS pour supprimer les espaces en debut et fin chaines
+    */
+    if(taskTitle.trim()){
+      addTask(taskTitle);
+      setTaskTitle("");
+      // console.log("envoie de la tache", taskTitle);
+    }
+    else {
+    //   console.log("Impossible une tache sans titre!");
+    // }
+  };
+
   return (
     <div className={`box ${styles.element}`}>
         <h2 className={styles.title}>🎯 Ajouter une tâche</h2>
-        <form action="" className={styles.container}>
+        <form action="" className={styles.container} onSubmit={handleAddtask}>
           <input 
+            required
             type="text" 
             name="" 
             id="" 
             className={styles.input}
             placeholder="Indiquer une nouvelle tache"
+            onChange={handleInputChange}
+            // l'attribut value nous permettra de reinitialiser le champ de saisie avec la methode setTaskTitle
+            value={taskTitle}
           />
+          {/* handle input change: gérer le changement de saisie */}
           <button type="submit" className="button-primary">Ajouter</button>
         </form>
     </div>
