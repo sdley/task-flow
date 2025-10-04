ad171f9c40d4fed7a6dcb9edfb777a95d8b01015
Souleymane DIALLO <sdley2020@gmail.com>
2025-10-04 16:32:46 +0000

    Updated Portfolio copyright url link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,42 +1,46 @@
 import styles from "./Footer.module.css";
 
-export const Footer = ({ completedTasks }) =>{
-    const currentYear = new Date().getFullYear();
+export const Footer = ({ completedTasks }) => {
+  const currentYear = new Date().getFullYear();
 
-    if (completedTasks){
-        return (
-            <footer>
-                <code className={styles.footer}>
-                    Avec TaskFlow, vous avez eliminé&nbsp; <span className="important">{completedTasks}</span> &nbsp;tâche
-                    {completedTasks > 1 ? 's' : ''}
-                    {/* {completedTasks > 1 ? 's' : null} */}
-                </code>
-                <p className={styles.footer}>
-                    Copyright ©️&nbsp;
-                    <a 
-                        href="https://sdley.github.io/" 
-                        target="_blank"> sdley </a>&nbsp; 
-                    {currentYear}
-                </p>
-    
-            </footer>
-        );
-    };
+  if (completedTasks) {
+    return (
+      <footer>
+        <code className={styles.footer}>
+          Avec TaskFlow, vous avez eliminé&nbsp;{" "}
+          <span className="important">{completedTasks}</span> &nbsp;tâche
+          {completedTasks > 1 ? "s" : ""}
+          {/* {completedTasks > 1 ? 's' : null} */}
+        </code>
+        <p className={styles.footer}>
+          Copyright ©️&nbsp;
+          <a href="https://sdley.github.io/" target="_blank">
+            {" "}
+            sdley{" "}
+          </a>
+          &nbsp;
+          {currentYear}
+        </p>
+      </footer>
+    );
+  }
 
-    /*
+  /*
     Note:
         En React on se doit d'eviter d'avoir des composants qui ne retournent rien.
         Pour palier cela, nous pouvons retourner null, ou une balise vide ou encore un frangment <></>.
         Cela evite les erreurs ou d'avoir des composants qui ne retounent rien !
         return null;
     */
-   return (
+  return (
     <div className={styles.footer}>
-        Copyright ©️&nbsp;
-        <a 
-            href="https://diallosouleymane.github.io/" 
-            target="_blank"> sdley </a>&nbsp; 
-        {currentYear}
+      Copyright ©️&nbsp;
+      <a href="https://sdley.github.io/" target="_blank">
+        {" "}
+        sdley{" "}
+      </a>
+      &nbsp;
+      {currentYear}
     </div>
-   );
-}
+  );
+};
